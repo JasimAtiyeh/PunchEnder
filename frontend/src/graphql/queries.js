8c93f7689e1df0343c08a95270cca3a6097a9606
Han Kim <joshhk72@gmail.com>
2019-11-09 16:54:27 -0500

    Add build form and related query

--- frontend/src/graphql/queries.js
+++ frontend/src/graphql/queries.js
@@ -26,8 +26,32 @@ export default {
       }
     }
   `,
+  FETCH_UNFINISHED_PROJECT: gql`
+    query FetchUnfinishedProject($_id: ID!) {
+      project(_id: $_id) {
+        _id
+        name
+        description
+        goal
+        endDate
+        rewards {
+          _id
+          name
+          pledgeAmount
+          tier
+        }
+        projectCreator {
+          _id
+        }  
+        category {
+          _id
+          name
+        }
+      }
+    }
+  `,
   FETCH_CATEGORIES: gql`
-    {
+    query FetchCategories {
       categories {
         _id
         name

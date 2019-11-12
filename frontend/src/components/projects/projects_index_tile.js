d3a5ce7e51c254765564db45dcc2b5b062d3e3c3
Jasim Atiyeh <jasimatiyeh@gmail.com>
2019-11-12 10:33:30 -0600

    work on project-index

--- frontend/src/components/projects/projects_index_tile.js
+++ frontend/src/components/projects/projects_index_tile.js
@@ -5,19 +5,19 @@ const ProjectIndexTile = props => {
   let percentFunded = (props.project.amountRaised / props.project.goal) * 100;
 
   return (
-    <div>
+    <div className='project-index-tile'>
       <Link to='/'>
-        <div>
+        <div className='project-index-tile-image'>
           <img src='https://punchender-dev.s3.us-east-2.amazonaws.com/StockSnap_Q1KHHDXXZT.jpg' height='100' width='100'/>
         </div>
-        <div>
+        <div className='project-index-tile-name'>
           {props.project.name}
         </div>
-        <div>
+        <div className='project-index-tile-funded'>
           {`${percentFunded}%`}
         </div>
-        <div>
-          {/* {props.project.projectCreator.name} */}
+        <div className='project-index-tile-project-creator'>
+          {props.project.projectCreator ? props.project.projectCreator.name : ''}
         </div>
       </Link>
     </div>

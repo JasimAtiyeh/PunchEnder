6ffcdaf5338444cb53519e856bd14e10801b2f61
Han Kim <joshhk72@gmail.com>
2019-11-13 13:41:05 -0500

    Readd keys_prod

---
+++ config/keys_prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  MONGO_URI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY,
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  region: process.env.REGION,
+  bucket: process.env.BUCKET
+}

7f4ef5ae3390f328922b55654524154b85aade84
Jasim Atiyeh <jasimatiyeh@gmail.com>
2019-11-12 23:35:11 -0600

    finish style for projects-index

--- frontend/src/components/projects/projects_index_tile.js
+++ frontend/src/components/projects/projects_index_tile.js
@@ -10,19 +10,21 @@ const ProjectIndexTile = props => {
         <div className='project-index-tile-image'>
           <img src={props.project.image || 'https://punchender-dev.s3.us-east-2.amazonaws.com/StockSnap_Q1KHHDXXZT.jpg'} alt={props.project.name} />
         </div>
-        <div>
+        <div className='project-index-tile-info'>
           <div className='project-index-tile-name'>
             {props.project.name}
           </div>
-          <div className='project-index-tile-funded'>
-            {`${percentFunded}%`}
-          </div>
-          <div className='project-index-tile-project-creator'>
-            {props.project.projectCreator ? props.project.projectCreator.name : ''}
+          <div className='project-index-tile-info-detail'>
+            <div className='project-index-tile-funded'>
+              {`${percentFunded}%`} funded
+            </div>
+            <div className='project-index-tile-project-creator'>
+              By {props.project.projectCreator ? props.project.projectCreator.name : ''}
+            </div>
           </div>
         </div>
-        <div>
-          <i class="material-icons">bookmark_border</i>
+        <div className='project-index-tile-bookmark'>
+          <i className="material-icons">bookmark_border</i>
         </div>
       </Link>
     </div>

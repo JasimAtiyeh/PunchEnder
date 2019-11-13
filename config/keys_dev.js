fd0e52f06a8d32ee63ace335d1a6d4138687e941
Han Kim <joshhk72@gmail.com>
2019-11-13 11:05:55 -0500

    Fix merge issue

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -2,8 +2,8 @@ module.exports = {
   MONGO_URI: "mongodb+srv://dev:e03DGUO9spOiaZhQ@cluster0-mejsw.mongodb.net/test?retryWrites=true&w=majority",
   secretOrKey: "#gx9A! /*/hSV173X8`w7}_D+h~VxPQ",
   // AWS keys here
-  accessKeyId: "AKIAI5GNBZO6XBBEUR5A",
-  secretAccessKey: "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",
+  accessKeyId: "AKIAJP7USPMKIPMPEJDQ",
+  secretAccessKey: "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",
   region: "us-east-1",
   bucket: "punchender-dev"
 };

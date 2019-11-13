a45dd6c8cd2fe776115eb3306f75523a15031c79
JasimAtiyeh <35203385+JasimAtiyeh@users.noreply.github.com>
2019-11-13 01:00:18 -0600

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  MONGO_URI: "mongodb+srv://dev:e03DGUO9spOiaZhQ@cluster0-mejsw.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "#gx9A! /*/hSV173X8`w7}_D+h~VxPQ",
-  // AWS keys here
-  accessKeyId: "AKIAI5GNBZO6XBBEUR5A",
-  secretAccessKey: "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",
-  region: "us-east-1"
-};

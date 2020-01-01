7eeb860cd151bce793108d48433796821d225f1a
Han Kim <joshhk72@gmail.com>
2019-12-31 21:03:21 -0500

    Add styling to pledge thank you page

--- frontend/src/components/pledge/confirm.js
+++ frontend/src/components/pledge/confirm.js
@@ -9,26 +9,29 @@ const PledgeConfirmation = props => {
 
   const { project } = initialState.pledgeProject;
   return (
-    <div className="pledge-thanks">
-      <h1>Congratulations!</h1>
-      <div className="pledge-thanks-top">
-        <img src={project.image}/>
-        <div>
-          You're a backer of <Link to={`/projects/${project._id}`}>{project.name}</Link>
+    <div className="pledge-thanks-page">
+      <div className="pledge-thanks">
+        <h1>Congratulations!</h1>
+        <div className="pledge-thanks-top">
+          <img src={project.image}/>
+          <div>
+            You're a backer of <Link to={`/projects/${project._id}`}>{project.name}</Link>
+          </div>
         </div>
+        <div className="pledge-thanks-summary">
+          <h2>Pledge summary</h2>
+          <div>
+            <em>AMOUNT PLEDGED</em>
+            <p>${initialState.pledgeProject.amount.toFixed(2)}</p>
+          </div>
+          <div>
+            <em>REWARD</em>
+            <p>{initialState.pledgeProject.reward ? initialState.pledgeProject.reward.name : "n/a"}</p>
+          </div>
+          <p className="pledge-thanks-note">Thank you for contributing to this project. We appreciate your charity and will use these funds to make the world a better place!</p>
+        </div>
+        <Link to={`/projects/${project._id}`}>Return to project page</Link>
       </div>
-      <div className="pledge-thanks-summary">
-        <h2>Pledge summary</h2>
-        <div>
-          <em>AMOUNT PLEDGED</em>
-          <p>${initialState.pledgeProject.amount.toFixed(2)}</p>
-        </div>
-        <div>
-          <em>REWARD</em>
-          <p>{initialState.pledgeProject.reward ? initialState.pledgeProject.reward.name : "n/a"}</p>
-        </div>
-      </div>
-      <Link to={`/projects/${project._id}`}>Return to project page</Link>
     </div>
   )
 };
